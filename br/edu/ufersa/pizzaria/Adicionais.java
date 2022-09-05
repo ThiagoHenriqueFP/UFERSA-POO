ff1405ea0ecc324b82a70eecfaf0ed3d754f063d
Vinicius Dantas <vinicius.dantas777@outlook.com>
2022-09-05 18:28:32 -0300

    classes order e pizzas

---
+++ br/edu/ufersa/pizzaria/Adicionais.java
@@ -0,0 +1,37 @@
+package edu.ufersa.pizzaria;
+
+class Adicionais {
+    private String name;
+    private double value;
+
+    void cadastrarAdicional(){
+        this.name = "Franchesco";
+        this.value = 1.00000000;
+    }
+
+    void setName(String name){
+        if(!name.isEmpty() && name != ""){ //Se nome está vazio e diferente de "" será adotado nome escrito
+            this.name = name;
+        }else{
+            System.out.println("Nome para o ADICIONAL é invalido...");
+        }
+    }
+
+    String getName(){return this.name;}
+
+    void setValor(double value){
+        if(value > 0 && value != 0){ //Se valor maior que 0 e diferente de 0 será adotado valor digitado
+            this.value = value;
+        }else{
+            System.out.println("Valor digitado para o ADICIONAL é invalido...");
+        }
+    }
+
+    double getValue(){return this.value;}
+
+    void cadastrarAdicional(String name, double value){
+        setName(name);
+        setValor(value);
+    }
+
+}
