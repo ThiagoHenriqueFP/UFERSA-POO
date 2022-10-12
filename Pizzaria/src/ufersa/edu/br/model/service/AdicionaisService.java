6be193e1816fcb7f17d7317badf931070513df7b
Whesley Xavier <xavierwhesley@gmail.com>
2022-10-12 19:51:33 -0300

    finish: AdditionalsService

--- Pizzaria/src/ufersa/edu/br/model/service/AdicionaisService.java
+++ Pizzaria/src/ufersa/edu/br/model/service/AdicionaisService.java
@@ -1,26 +1,81 @@
-package ufersa.edu.br.model.service;
+package br.edu.ufersa.pizzaria.model.services;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import ufersa.edu.br.model.dao.AdicionaisDao;
+import br.edu.ufersa.pizzaria.model.dao.AdicionaisDAO;
+import br.edu.ufersa.pizzaria.model.dao.BaseInterDAO;
+import br.edu.ufersa.pizzaria.model.entities.Adicionais;
 
 public class AdicionaisService {
+
     BaseInterDAO<Adicionais> dao = new AdicionaisDAO();
 
-    public boolean insertAdditionais(Adicionais adicionais){
+    public boolean insertAdditionais(Adicionais adicionais) {
         ResultSet rs = dao.findAdicional(adicionais);
-        try{
-            if(rs == null || !(rs.next())){
-                if(dao.insertAdicional(adicionais) == true){
+        try {
+            if (rs == null || !(rs.next())) {
+                if (dao.insertAdicional(adicionais) == true) {
                     return true;
-                }else{
+                } else {
                     return false;
                 }
             }
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-			return false;
+            return false;
         }
     }
+
+    public List<Adicionais> getAllAdicionais() {
+        List<Adicionais> addresses = new ArrayList<Adicionais>();
+        ResultSet rs = dao.getAll();
+        try {
+            while (rs.next()) {
+                Adicionais adicionais = new Adicionais();
+                adicionais.setName(rs.getString("name"));
+                adicionais.setValue(rs.getDouble("value"));
+
+                adicionais.add(adicionais);
+            }
+            return adicionais;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean editAdicionais(Adicionais adicionais) {
+        ResultSet rs = dao.findBySpecifiedField(adicionais, "name");
+        try {
+            if (rs != null && rs.next()) {
+                if (dao.edit(adicionais) == true) {
+                    return true;
+                } else
+                    return false;
+            } else
+                return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteAdicionais(Adicionais adicionais) {
+        ResultSet rs = dao.findBySpecifiedField(adicionais, "name");
+        try {
+            if (rs != null && rs.next()) {
+                if (dao.delete(adicionais) == true) {
+                    return true;
+                } else
+                    return false;
+            } else
+                return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
